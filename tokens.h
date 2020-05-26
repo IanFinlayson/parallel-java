5f178d8d8004e9ae406ee1fe17554e026b0c1703
Ian Finlayson <ifinlay@umw.edu>
2020-05-26 16:09:14 +0000

    Add a sample java program, program to return its tokens, and list of token definitions

---
+++ tokens.h
@@ -0,0 +1,115 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+// identifiers and literals
+#define TOK_IDENTIFIER 100
+#define TOK_INTVAL 101
+#define TOK_FLOATVAL 102
+#define TOK_STRINGVAL 103
+#define TOK_BOOLVAL 104
+#define TOK_CHARVAL 105
+#define TOK_NULLVAL 106
+
+// keywords
+#define TOK_ABSTRACT 200
+#define TOK_CONTINUE 201
+#define TOK_FOR 202
+#define TOK_NEW 203
+#define TOK_SWITCH 204
+#define TOK_ASSERT 205
+#define TOK_DEFAULT 206
+#define TOK_GOTO 207
+#define TOK_PACKAGE 208
+#define TOK_SYNCHRONIZED 209
+#define TOK_BOOLEAN 210
+#define TOK_DO 211
+#define TOK_IF 212
+#define TOK_PRIVATE 213
+#define TOK_THIS 214
+#define TOK_BREAK 215
+#define TOK_DOUBLE 216
+#define TOK_IMPLEMENTS 217
+#define TOK_PROTECTED 218
+#define TOK_THROW 219
+#define TOK_BYTE 220
+#define TOK_ELSE 221
+#define TOK_IMPORT 222
+#define TOK_PUBLIC 223
+#define TOK_THROWS 224
+#define TOK_CASE 225
+#define TOK_ENUM 226
+#define TOK_INSTANCEOF 227
+#define TOK_RETURN 228
+#define TOK_TRANSIENT 229
+#define TOK_CATCH 230
+#define TOK_EXTENDS 231
+#define TOK_INT 232
+#define TOK_SHORT 233
+#define TOK_TRY 234
+#define TOK_CHAR 235
+#define TOK_FINAL 236
+#define TOK_INTERFACE 237
+#define TOK_STATIC 238
+#define TOK_VOID 239
+#define TOK_CLASS 240
+#define TOK_FINALLY 241
+#define TOK_LONG 242
+#define TOK_STRICTFP 243
+#define TOK_VOLATILE 244
+#define TOK_CONST 245
+#define TOK_FLOAT 246
+#define TOK_NATIVE 247
+#define TOK_SUPER 248
+#define TOK_WHILE 249
+
+// punctuation
+#define TOK_LBRACKET 300
+#define TOK_RBRACKET 301
+#define TOK_LPAREN 302
+#define TOK_RPAREN 303
+#define TOK_LBRACE 304
+#define TOK_RBRACE 305
+#define TOK_COMMA 306
+#define TOK_SEMI 307
+#define TOK_DOT 308
+#define TOK_ASSIGN 309
+#define TOK_MODASSIGN 310
+#define TOK_DIVASSIGN 311
+#define TOK_MULASSIGN 312
+#define TOK_ADDASSIGN 313
+#define TOK_SUBASSIGN 314
+#define TOK_COLON 315
+#define TOK_QUESTION 316
+#define TOK_OR 317
+#define TOK_AND 318
+#define TOK_BITOR 319
+#define TOK_BITAND 320
+#define TOK_BITXOR 321
+#define TOK_EQUAL 322
+#define TOK_NEQUAL 323
+#define TOK_LESS 324
+#define TOK_LEQUAL 325
+#define TOK_GREATER 326
+#define TOK_GEQUAL 327
+#define TOK_LSHIFT 328
+#define TOK_RSHIFT 329
+#define TOK_RSHIFTZ 330
+#define TOK_ADD 331
+#define TOK_SUB 332
+#define TOK_MOD 333
+#define TOK_DIV 334
+#define TOK_MUL 335
+#define TOK_BITNEG 336
+#define TOK_NEG 337
+#define TOK_SUBSUB 338
+#define TOK_ADDADD 339
+
+#endif
+
+
+public class Sample {
+    public static void main(String args[]) {
+        System.out.println("Hello World!"); 
+    }
+}
+
