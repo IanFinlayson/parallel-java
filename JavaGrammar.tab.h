8b2654719230e2a6d723b419f1e1723b5dcddd12
keshyra <ksmith33@mail.umw.edu>
2020-05-29 15:58:20 +0000

    Revert "Added a "code requires" section. Fixed prologue to work with lex file"
    This reverts commit 12468a8d24dc98cb43c28594c1574d3fe1fee5d9.

---
+++ JavaGrammar.tab.h
@@ -0,0 +1,161 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_JAVAGRAMMAR_TAB_H_INCLUDED
+# define YY_YY_JAVAGRAMMAR_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    TOK_IDENTIFIER = 100,
+    TOK_INTVAL = 101,
+    TOK_FLOATVAL = 102,
+    TOK_STRINGVAL = 103,
+    TOK_BOOLVAL = 104,
+    TOK_CHARVAL = 105,
+    TOK_NULLVAL = 106,
+    TOK_ABSTRACT = 200,
+    TOK_CONTINUE = 201,
+    TOK_FOR = 202,
+    TOK_NEW = 203,
+    TOK_SWITCH = 204,
+    TOK_ASSERT = 205,
+    TOK_DEFAULT = 206,
+    TOK_GOTO = 207,
+    TOK_PACKAGE = 208,
+    TOK_SYNCHRONIZED = 209,
+    TOK_BOOLEAN = 210,
+    TOK_DO = 211,
+    TOK_IF = 212,
+    TOK_PRIVATE = 213,
+    TOK_THIS = 214,
+    TOK_BREAK = 215,
+    TOK_DOUBLE = 216,
+    TOK_IMPLEMENTS = 217,
+    TOK_PROTECTED = 218,
+    TOK_THROW = 219,
+    TOK_BYTE = 220,
+    TOK_ELSE = 221,
+    TOK_IMPORT = 222,
+    TOK_PUBLIC = 223,
+    TOK_THROWS = 224,
+    TOK_CASE = 225,
+    TOK_ENUM = 226,
+    TOK_INSTANCEOF = 227,
+    TOK_RETURN = 228,
+    TOK_TRANSIENT = 229,
+    TOK_CATCH = 230,
+    TOK_EXTENDS = 231,
+    TOK_INT = 232,
+    TOK_SHORT = 233,
+    TOK_TRY = 234,
+    TOK_CHAR = 235,
+    TOK_FINAL = 236,
+    TOK_INTERFACE = 237,
+    TOK_STATIC = 238,
+    TOK_VOID = 239,
+    TOK_CLASS = 240,
+    TOK_FINALLY = 241,
+    TOK_LONG = 242,
+    TOK_STRICTFP = 243,
+    TOK_VOLATILE = 244,
+    TOK_CONST = 245,
+    TOK_FLOAT = 246,
+    TOK_NATIVE = 247,
+    TOK_SUPER = 248,
+    TOK_WHILE = 249,
+    TOK_STRING = 250,
+    TOK_LBRACKET = 300,
+    TOK_RBRACKET = 301,
+    TOK_LPAREN = 302,
+    TOK_RPAREN = 303,
+    TOK_LBRACE = 304,
+    TOK_RBRACE = 305,
+    TOK_COMMA = 306,
+    TOK_SEMI = 307,
+    TOK_DOT = 308,
+    TOK_ASSIGN = 309,
+    TOK_MODASSIGN = 310,
+    TOK_DIVASSIGN = 311,
+    TOK_MULASSIGN = 312,
+    TOK_ADDASSIGN = 313,
+    TOK_SUBASSIGN = 314,
+    TOK_COLON = 315,
+    TOK_QUESTION = 316,
+    TOK_OR = 317,
+    TOK_AND = 318,
+    TOK_BITOR = 319,
+    TOK_BITAND = 320,
+    TOK_BITXOR = 321,
+    TOK_EQUAL = 322,
+    TOK_NEQUAL = 323,
+    TOK_LESS = 324,
+    TOK_LEQUAL = 325,
+    TOK_GREATER = 326,
+    TOK_GEQUAL = 327,
+    TOK_LSHIFT = 328,
+    TOK_RSHIFT = 329,
+    TOK_RSHIFTZ = 330,
+    TOK_ADD = 331,
+    TOK_SUB = 332,
+    TOK_MOD = 333,
+    TOK_DIV = 334,
+    TOK_MUL = 335,
+    TOK_BITNEG = 336,
+    TOK_NEG = 337,
+    TOK_SUBSUB = 338,
+    TOK_ADDADD = 339
+  };
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_JAVAGRAMMAR_TAB_H_INCLUDED  */
--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample {
     public static void main(String args[]) {
-        int x = 5; 
+        System.out.println("Hello World!"); 
     }
 }
 
